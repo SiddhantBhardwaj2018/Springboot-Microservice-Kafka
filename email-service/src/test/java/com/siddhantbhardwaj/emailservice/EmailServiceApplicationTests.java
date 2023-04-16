eb916f46520e30066cbf802718a1265864190c29
SiddhantBhardwaj2018 <bhardwajsiddhant03@gmail.com>
2023-04-16 12:49:14 +0530

    Adding completed microservics with docker images of zookeeper and kafka broker

---
+++ email-service/src/test/java/com/siddhantbhardwaj/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddhantbhardwaj.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order-service/src/test/java/com/siddhantbhardwaj/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddhantbhardwaj.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
